8477b2dfec290456d0d5f00de20742f234115b8f
Christopher De Cairos <cade@mozillafoundation.org>
2014-10-29 16:46:31 -0400

    implement login 3.0

--- server/index.js
+++ server/index.js
@@ -20,10 +20,15 @@ module.exports = function(config) {
   app.use(login.cookieSession());
 
   app.post('/auth/authenticate', login.handlers.authenticate);
-  app.post('/auth/check-username', login.handlers.exists);
-  app.post('/auth/create', login.handlers.create);
   app.post('/auth/logout', login.handlers.logout);
   app.post('/auth/verify', login.handlers.verify);
+  app.post('/auth/v2/create', login.handlers.createUser);
+  app.post('/auth/v2/uid-exists', login.handlers.uidExists);
+  app.post('/auth/v2/request', login.handlers.request);
+  app.post('/auth/v2/authenticateToken', login.handlers.authenticateToken);
+  app.post('/auth/v2/verify-password', login.handlers.verifyPassword);
+  app.post('/auth/v2/request-reset-code', login.handlers.requestResetCode);
+  app.post('/auth/v2/reset-password', login.handlers.resetPassword);
 
   app.use('/', express.static(path.join(__dirname, '../.server')));
 
